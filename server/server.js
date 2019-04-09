687eb046a4bc4828e38cac7336ec3369e974a0d3
Fatai Balogun <fattylee.remod@gmail.com>
2019-04-09 22:15:56 +0100

    add express module and public asset

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
 const port = process.env.PORT || 5000;
 
