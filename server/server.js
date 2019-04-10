8c0273eed1c6d9bece0547b65264cabb51a1cfb2
Fatai Balogun <fattylee.remod@gmail.com>
2019-04-10 22:01:06 +0100

    add event listeners for the client and server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use('/', express.static(path.join(__dirname, '..', 'public')));
 
+
+io.on('connection', (socket) => {
+  console.log('new user conected!');
+  
+  socket.on('disconnect', () => {
+    console.log('client disconected');
+  });
+});
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server running on port', port);
 });
