aa42bb1c89adf01d8e9f79e687e414ee0b482048
Fatai Balogun <fattylee.remod@gmail.com>
2019-04-10 23:41:39 +0100

    add io emitter for sending messages to all connected users

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 const socket = io();
-socket.on('connect', () => console.log('connected to server'));
+socket.on('connect', () => {
+  console.log('connected to server');
+  socket.on('newEmail', (payload) => {
+    console.log('Got new email', payload);
+  });
+  
+  socket.emit('createEmail', {
+    to: 'mapopo',
+    text: 'hey, maposki!',
+  });
+  
+  socket.on('newMessage', (payload) => {
+    console.log('got a new message from:', payload);
+  });
+  
+  socket.emit('createMessage', {from: 'fattylee', 'text': 'Good-day everyone!'});
+
+});
 
 socket.on('disconnect', () => {
-  console.log('disconnected from server');  
+  console.log('disconnected from server, ure now offline');  
 });
--- server/server.js
+++ server/server.js
@@ -16,6 +16,29 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('client disconected');
   });
+  
+  socket.emit('newEmail', {
+    from: 'mapopo',
+    text: 'hi, fattylee',
+    createdAt: Date.now()
+  });
+  
+  socket.on('createEmail', (payload) => {
+    payload.createdAt = 123;
+    console.log('send email to:', payload);
+  });
+  
+  
+  
+  socket.on('createMessage', (payload) => {
+    payload.createdAt = Date.now();
+    console.log('dispatching a new message');
+    io.emit('newMessage', payload);/* {
+    from: 'haleemath',
+    text: 'hi, everyone hope youre catching fun?!',
+    createdAt: Date.now()
+  });*/
+  });
 });
 
 const port = process.env.PORT || 5000;
