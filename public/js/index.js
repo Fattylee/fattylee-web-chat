7bc6c8c83057e9bf8e913f14c509c6cd15cdc509
Fatai Balogun <fattylee.remod@gmail.com>
2019-04-11 09:07:29 +0100

    implement welcome and new user alert message

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,13 @@
 const socket = io();
 socket.on('connect', () => {
   console.log('connected to server');
-  socket.on('newEmail', (payload) => {
-    console.log('Got new email', payload);
-  });
   
-  socket.emit('createEmail', {
-    to: 'mapopo',
-    text: 'hey, maposki!',
-  });
-  
-  socket.on('newMessage', (payload) => {
-    console.log('got a new message from:', payload);
-  });
-  
-  socket.emit('createMessage', {from: 'fattylee', 'text': 'Good-day everyone!'});
-
 });
 
 socket.on('disconnect', () => {
   console.log('disconnected from server, ure now offline');  
+});
+
+socket.on('newMessage', (payload) => {
+  console.log('newMessage', payload);
 });
--- server/server.js
+++ server/server.js
@@ -17,28 +17,17 @@ io.on('connection', (socket) => {
     console.log('client disconected');
   });
   
-  socket.emit('newEmail', {
-    from: 'mapopo',
-    text: 'hi, fattylee',
-    createdAt: Date.now()
-  });
-  
-  socket.on('createEmail', (payload) => {
-    payload.createdAt = 123;
-    console.log('send email to:', payload);
-  });
-  
-  
-  
+  socket.emit('newMessage', {from: 'admin', text: 'welcome to the chat room', createdAt: new Date().getTime()});
+  socket.broadcast.emit('newMessage', {from: 'admin', 'text': 'a new user just joined the chat room', createdAt: Date.now()});
   socket.on('createMessage', (payload) => {
     payload.createdAt = Date.now();
+    
     console.log('dispatching a new message');
-    io.emit('newMessage', payload);/* {
-    from: 'haleemath',
-    text: 'hi, everyone hope youre catching fun?!',
-    createdAt: Date.now()
-  });*/
+    //io.emit('newMessage', payload);
+    socket.broadcast.emit('newMessage', payload);
   });
+  
+  
 });
 
 const port = process.env.PORT || 5000;
